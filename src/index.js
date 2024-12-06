fabcd31e23de799d7fe14a2b3084fb11bf3df0ff
kawtar <kawtarbenaachour1@gmail.com>
2024-12-06 21:40:37 +0100

    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, World! CI/CD Pipeline is working!');
+    res.end('Hello, World! CI/CD Pipeline is working kawtar!');
 });
 
 const PORT = process.env.PORT || 3000;
